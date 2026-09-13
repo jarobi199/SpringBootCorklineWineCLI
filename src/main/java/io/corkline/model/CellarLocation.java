1b2d75479e633be314c05b5833602598aa83f55e
derrickpryce <dpryce7@gmail.com>
2026-09-13 04:46:38 -0400

    Another commit.

---
+++ src/main/java/io/corkline/model/CellarLocation.java
@@ -0,0 +1,90 @@
+package io.corkline.model;
+
+import io.corkline.enums.StorageType;
+import org.springframework.data.annotation.Id;
+import org.springframework.data.mongodb.core.mapping.Document;
+
+import java.util.ArrayList;
+import java.util.List;
+
+@Document(collection = "cellar_locations")
+public class CellarLocation {
+    @Id
+    private String id;
+    private String description;
+    private StorageType storageType;
+    private int  capacity;
+    private Range idealTemperatureC;
+    private Range idealHumidityPercent;
+    private List<ConditionReading> readings;
+
+    public CellarLocation() {
+        //No argument constructor
+    }
+
+    public CellarLocation(String description, StorageType storageType, int capacity, Range idealTemperatureC,
+                          Range idealHumidityPercent) {
+        this.description = description;
+        this.storageType = storageType;
+        this.capacity = capacity;
+        this.idealTemperatureC = idealTemperatureC;
+        this.idealHumidityPercent = idealHumidityPercent;
+        this.readings = new ArrayList<>();
+    }
+
+    public String getId() {
+        return id;
+    }
+
+    public void setId(String id) {
+        this.id = id;
+    }
+
+    public String getDescription() {
+        return description;
+    }
+
+    public void setDescription(String description) {
+        this.description = description;
+    }
+
+    public StorageType getStorageType() {
+        return storageType;
+    }
+
+    public void setStorageType(StorageType storageType) {
+        this.storageType = storageType;
+    }
+
+    public int getCapacity() {
+        return capacity;
+    }
+
+    public void setCapacity(int capacity) {
+        this.capacity = capacity;
+    }
+
+    public Range getIdealTemperatureC() {
+        return idealTemperatureC;
+    }
+
+    public void setIdealTemperatureC(Range idealTemperatureC) {
+        this.idealTemperatureC = idealTemperatureC;
+    }
+
+    public Range getIdealHumidityPercent() {
+        return idealHumidityPercent;
+    }
+
+    public void setIdealHumidityPercent(Range idealHumidityPercent) {
+        this.idealHumidityPercent = idealHumidityPercent;
+    }
+
+    public List<ConditionReading> getReadings() {
+        return readings;
+    }
+
+    public void setReadings(List<ConditionReading> readings) {
+        this.readings = readings;
+    }
+}
--- src/main/java/io/corkline/model/ConditionReading.java
+++ src/main/java/io/corkline/model/ConditionReading.java
@@ -1,47 +1,5 @@
 package io.corkline.model;
 
-public class ConditionReading {
-    private String recordedAt;
-    private int temperatureC;
-    private int humidityPercent;
-    private String note;
+import java.time.LocalDateTime;
 
-    public ConditionReading(String recordedAt, int temperatureC, int humidityPercent, String note) {
-        this.recordedAt = recordedAt;
-        this.temperatureC = temperatureC;
-        this.humidityPercent = humidityPercent;
-        this.note = note;
-    }
-
-    public String getRecordedAt() {
-        return recordedAt;
-    }
-
-    public void setRecordedAt(String recordedAt) {
-        this.recordedAt = recordedAt;
-    }
-
-    public int getTemperatureC() {
-        return temperatureC;
-    }
-
-    public void setTemperatureC(int temperatureC) {
-        this.temperatureC = temperatureC;
-    }
-
-    public int getHumidityPercent() {
-        return humidityPercent;
-    }
-
-    public void setHumidityPercent(int humidityPercent) {
-        this.humidityPercent = humidityPercent;
-    }
-
-    public String getNote() {
-        return note;
-    }
-
-    public void setNote(String note) {
-        this.note = note;
-    }
-}
+public record ConditionReading (LocalDateTime dateTime, int temperatureC, int humidityPercent, String note) {}
